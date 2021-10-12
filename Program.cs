4267f0aa88220ee2754eaed30295a4ee90ecd4ce
FRANKLIN STANLEY LARIN SARAVIA <USIS002421@ugb.edu.sv>
2021-10-12 14:11:04 -0600

    login aun en proceso

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace L0GIN
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form2()); 
         }
     }
 }
