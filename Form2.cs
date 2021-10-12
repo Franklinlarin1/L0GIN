c849dd76268d6a2f3b33856d8ee1df072868c7f0
FRANKLIN STANLEY LARIN SARAVIA <USIS002421@ugb.edu.sv>
2021-10-12 14:09:34 -0600

    Agregar archivos de proyecto.

---
+++ Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace L0GIN
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void inicarSecionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+        }
+
+        private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("En mantenimiento");
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+MessageBox.Show ("En mantenimiento");
+        }
+    }
+}
